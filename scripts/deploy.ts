cf98e2b05fe6196497c87336344407b78515d698
Saruman <saruman@golom.io>
2022-09-03 22:52:06 +0400

    trading and deployment scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,9 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from 'hardhat';
+// import { ethers } from 'hardhat';
+
+const hre = require('hardhat');
 
 async function main() {
     /**
@@ -11,32 +13,62 @@ async function main() {
      *  1. deploy timelock
      */
 
-    const GOVERNANCE = '0x9aB76D1181c7723Ce4ab480Fb9610eFF7C6865Fa';
-    const GENESIS_START_TIME = '1653508800'; // 25th May 2022, 00.00 AM GST
+    // deploy golom token
+    // deploy vescrow
+    // deploy reward distributor
+    // deploy trader
+    // set reward distributor on trader
+    // set minter on golom token to reward distributor
 
-    const GolomTrader = await ethers.getContractFactory('GolomTrader');
-    const GolomToken = await ethers.getContractFactory('GolomToken');
-    const RewardDistributor = await ethers.getContractFactory('RewardDistributor');
 
-    const golomTrader = await GolomTrader.deploy(GOVERNANCE);
+    const GOVERNANCE = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+    const GENESIS_START_TIME = '1662321600'; // 25th May 2022, 00.00 AM GST
+
+    const GolomTrader = await hre.ethers.getContractFactory('GolomTrader');
+    const GolomToken = await hre.ethers.getContractFactory('GolomToken');
+    const RewardDistributor = await hre.ethers.getContractFactory('RewardDistributor');
+
+    const Lib = await hre.ethers.getContractFactory("TokenUriHelper");
+    const lib = await Lib.deploy();
+    await lib.deployed();
+
+    const VoteEscrowDelegation = await hre.ethers.getContractFactory('VoteEscrow', {
+        libraries: {
+            TokenUriHelper: lib.address,
+        },
+      });
+
+    const Weth = await hre.ethers.getContractFactory('WETH');
+    const weth = await Weth.deploy();
+  
+    const golomTrader = await GolomTrader.deploy(GOVERNANCE,weth.address);
     const golomToken = await GolomToken.deploy(GOVERNANCE);
 
-    const rewardDistributor = await RewardDistributor.deploy(
-        GENESIS_START_TIME,
-        golomTrader.address,
-        golomToken.address,
-        GOVERNANCE
+    const voteEscrowDelegation = await VoteEscrowDelegation.deploy(
+        golomToken.address
     );
 
-    // should be called by governance (considering msg.sender == governance)
+    const rewardDistributor = await RewardDistributor.deploy(
+        weth.address,
+        golomTrader.address,
+        golomToken.address,
+        GOVERNANCE,
+        voteEscrowDelegation.address,
+        GENESIS_START_TIME
+    );
+
     await golomToken.setMinter(rewardDistributor.address);
-    await golomToken.executeSetMinter();
     await golomTrader.setDistributor(rewardDistributor.address);
+
+    await golomToken.mintAirdrop(rewardDistributor.address);
+    await golomToken.mintGenesisReward(rewardDistributor.address);
+
     console.log(`🎉🎉🎉 Deployment Successful 🎉🎉🎉 `);
     console.log({
         GolomTrader: golomTrader.address,
         GolomToken: golomToken.address,
         RewardDistributor: rewardDistributor.address,
+        VeEscrow: voteEscrowDelegation.address,
     });
 }
 
--- scripts/tradertest.js
+++ scripts/tradertest.js
@@ -36,28 +36,59 @@ async function main() {
     const [owner, addr1, addr2, addr3] = await hre.ethers.getSigners();
 
     // deployment script
-    const rewardDistributor = await hre.ethers.getContractFactory('RewardDistributor');
-    const Funnel = await hre.ethers.getContractFactory('Funnel');
-    const funnel = await Funnel.deploy();
-    const rewards_starts_at = 1645347600 + 50 * 24 * 3600;
-    const Trader = await hre.ethers.getContractFactory('Molotrader');
-    const trader = await Trader.deploy();
+    const GOVERNANCE = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+    const GENESIS_START_TIME = '1662321600'; // 25th May 2022, 00.00 AM GST
 
-    await trader.deployed();
-    const Ve = await hre.ethers.getContractFactory('ve');
-    const ve = await Ve.deploy(funnel.address);
+    const Weth = await hre.ethers.getContractFactory('WETH');
+    const weth = await Weth.deploy();
 
-    console.log('addr', trader.address, funnel.address);
-    const rewarddistributor = await rewardDistributor.deploy(rewards_starts_at, trader.address, funnel.address, ve.address);
+    const GolomTrader = await hre.ethers.getContractFactory('GolomTrader');
+    const GolomToken = await hre.ethers.getContractFactory('GolomToken');
+    const RewardDistributor = await hre.ethers.getContractFactory('RewardDistributor');
 
-    console.log(rewarddistributor.address, await rewarddistributor.epoch(), await rewarddistributor.trader());
-    trader.setDistributor(rewarddistributor.address);
-    funnel.setMinter(rewarddistributor.address);
+    const Lib = await hre.ethers.getContractFactory("TokenUriHelper");
+    const lib = await Lib.deploy();
+    await lib.deployed();
 
-    await hre.ethers.provider.send('evm_setNextBlockTimestamp', [rewards_starts_at]);
-    await hre.ethers.provider.send('evm_mine');
-    // hre.ethers.provider.send("evm_mine"); // mine the next block
-    const GameItem = await hre.ethers.getContractFactory('GameItem');
+    const VoteEscrowDelegation = await hre.ethers.getContractFactory('VoteEscrow', {
+        libraries: {
+            TokenUriHelper: lib.address,
+        },
+      });
+
+    const golomTrader = await GolomTrader.deploy(GOVERNANCE,weth.address);
+    const golomToken = await GolomToken.deploy(GOVERNANCE);
+
+    const voteEscrowDelegation = await VoteEscrowDelegation.deploy(
+        golomToken.address
+    );
+
+    const rewardDistributor = await RewardDistributor.deploy(
+        weth.address,
+        golomTrader.address,
+        golomToken.address,
+        GOVERNANCE,
+        voteEscrowDelegation.address,
+        GENESIS_START_TIME
+    );
+
+    await golomToken.setMinter(rewardDistributor.address);
+    await golomTrader.setDistributor(rewardDistributor.address);
+
+    await golomToken.mintAirdrop(rewardDistributor.address);
+    await golomToken.mintGenesisReward(rewardDistributor.address);
+
+    console.log(`🎉🎉🎉 Deployment Successful 🎉🎉🎉 `);
+    console.log({
+        GolomTrader: golomTrader.address,
+        GolomToken: golomToken.address,
+        RewardDistributor: rewardDistributor.address,
+        VeEscrow: voteEscrowDelegation.address,
+    });
+
+    const trader = golomTrader
+
+    const GameItem = await hre.ethers.getContractFactory('ERC721Mock');
     var gameitem = await GameItem.deploy();
 
     var current = await gameitem.connect(addr1).setApprovalForAll(trader.address, true);
@@ -66,7 +97,7 @@ async function main() {
     var current = await gameitem.connect(addr2).setApprovalForAll(trader.address, true);
     await current.wait();
 
-    var receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    var receipt = await (await gameitem.mint(addr1.address)).wait();
     var tokenid = parseInt(receipt.events[0].args[2]);
 
     const domain = {
@@ -112,17 +143,24 @@ async function main() {
     var postPay = 0;
     //   const recoveredAddress = hre.ethers.utils.verifyTypedData(domain, types, order, signature2);
     //   console.log(recoveredAddress)
+
+    // Order calldata o,
+    // uint256 amount,
+    // address referrer,
+    // Payment calldata p,
+    // address receiver
+
     //   // var signedMatch = ["0x30917a657ae7d1132bdca40187d781fa3b60002f",2608,"0x55ca81f5f00dee4a072f793d67296abd6b56ba0b",100000000000,[1000000,"0xcd105202276e97b531065a087cecf8f0b76ab737"],[1000000,"0xcd105202276e97b531065a087cecf8f0b76ab737"],true,1000000,20,1655555,27,"0xd3dc3475099d1f59fa88de9d0d547a6b26a5e38fb210f0bdded377e089d3eb5c","0xd3dc3475099d1f59fa88de9d0d547a6b26a5e38fb210f0bdded377e089d3eb5c"]
-    var d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'], {
+    var d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'],addr2.address, {
         value: totoamt + postPay
     });
 
     receipt = await d.wait();
-    console.log(receipt.cumulativeGasUsed);
+    console.log(parseInt(receipt.cumulativeGasUsed));
     // console.log(await rewarddistributor.epoch());
     // console.log(await rewarddistributor.epochTotalFee(1));
     // console.log(await rewarddistributor.epochTotalFee(0));
-    receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    receipt = await (await gameitem.mint(addr1.address)).wait();
     tokenid = parseInt(receipt.events[0].args[2]);
     order.tokenId = tokenid;
     signature2 = await addr1._signTypedData(domain, types, order);
@@ -130,13 +168,13 @@ async function main() {
     order.r = '0x' + signature.substring(0, 64);
     order.s = '0x' + signature.substring(64, 128);
     order.v = parseInt(signature.substring(128, 130), 16);
-    var d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'], {
+    var d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'],addr2.address, {
         value: totoamt + postPay
     });
     receipt = await d.wait();
-    console.log(receipt.cumulativeGasUsed);
+    console.log(parseInt(receipt.cumulativeGasUsed));
 
-    receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    receipt = await (await gameitem.mint(addr1.address)).wait();
     tokenid = parseInt(receipt.events[0].args[2]);
     order.tokenId = tokenid;
     signature2 = await addr1._signTypedData(domain, types, order);
@@ -146,16 +184,16 @@ async function main() {
     order.v = parseInt(signature.substring(128, 130), 16);
     // console.log(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16']);
 
-    d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'], {
+    d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'],addr2.address, {
         value: totoamt + postPay
     });
     receipt = await d.wait();
 
     await hre.ethers.provider.send('evm_increaseTime', [3600 * 25]);
     await hre.ethers.provider.send('evm_mine');
-    console.log(receipt.cumulativeGasUsed);
+    console.log(parseInt(receipt.cumulativeGasUsed));
 
-    receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    receipt = await (await gameitem.mint(addr1.address)).wait();
     tokenid = parseInt(receipt.events[0].args[2]);
     order.tokenId = tokenid;
     signature2 = await addr1._signTypedData(domain, types, order);
@@ -163,11 +201,11 @@ async function main() {
     order.r = '0x' + signature.substring(0, 64);
     order.s = '0x' + signature.substring(64, 128);
     order.v = parseInt(signature.substring(128, 130), 16);
-    d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'], {
+    d = await trader.connect(addr2).fillAsk(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16'],addr2.address, {
         value: totoamt + postPay
     });
     receipt = await d.wait();
-    console.log(receipt.cumulativeGasUsed, await funnel.balanceOf(addr1.address));
+    console.log(parseInt(receipt.cumulativeGasUsed));
 
     // await rewarddistributor.multiTraderClaim(addr1.address, [1]);
     // console.log(await funnel.balanceOf(addr1.address));
@@ -180,12 +218,59 @@ async function mainbid() {
     const null_address = '0x0000000000000000000000000000000000000000';
     const [owner, addr1, addr2, addr3] = await hre.ethers.getSigners();
 
-    const Trader = await hre.ethers.getContractFactory('Molotrader');
-    const trader = await Trader.deploy();
+    const GOVERNANCE = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+    const GENESIS_START_TIME = '1662321600'; // 25th May 2022, 00.00 AM GST
 
-    await trader.deployed();
+    const Weth = await hre.ethers.getContractFactory('WETH');
+    const weth = await Weth.deploy();
 
-    const GameItem = await hre.ethers.getContractFactory('GameItem');
+    const GolomTrader = await hre.ethers.getContractFactory('GolomTrader');
+    const GolomToken = await hre.ethers.getContractFactory('GolomToken');
+    const RewardDistributor = await hre.ethers.getContractFactory('RewardDistributor');
+
+    const Lib = await hre.ethers.getContractFactory("TokenUriHelper");
+    const lib = await Lib.deploy();
+    await lib.deployed();
+
+    const VoteEscrowDelegation = await hre.ethers.getContractFactory('VoteEscrow', {
+        libraries: {
+            TokenUriHelper: lib.address,
+        },
+      });
+
+    const golomTrader = await GolomTrader.deploy(GOVERNANCE,weth.address);
+    const golomToken = await GolomToken.deploy(GOVERNANCE);
+
+    const voteEscrowDelegation = await VoteEscrowDelegation.deploy(
+        golomToken.address
+    );
+
+    const rewardDistributor = await RewardDistributor.deploy(
+        weth.address,
+        golomTrader.address,
+        golomToken.address,
+        GOVERNANCE,
+        voteEscrowDelegation.address,
+        GENESIS_START_TIME
+    );
+
+    await golomToken.setMinter(rewardDistributor.address);
+    await golomTrader.setDistributor(rewardDistributor.address);
+
+    await golomToken.mintAirdrop(rewardDistributor.address);
+    await golomToken.mintGenesisReward(rewardDistributor.address);
+
+    console.log(`🎉🎉🎉 Deployment Successful 🎉🎉🎉 `);
+    console.log({
+        GolomTrader: golomTrader.address,
+        GolomToken: golomToken.address,
+        RewardDistributor: rewardDistributor.address,
+        VeEscrow: voteEscrowDelegation.address,
+    });
+
+    const trader = golomTrader
+
+    const GameItem = await hre.ethers.getContractFactory('ERC721Mock');
     var gameitem = await GameItem.deploy();
 
     var current = await gameitem.connect(addr1).setApprovalForAll(trader.address, true);
@@ -194,11 +279,10 @@ async function mainbid() {
     current = await gameitem.connect(addr2).setApprovalForAll(trader.address, true);
     await current.wait();
 
-    var receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    var receipt = await (await gameitem.mint(addr1.address)).wait();
     var tokenid = parseInt(receipt.events[0].args[2]);
 
-    let wethContract = await hre.ethers.getContractAt('WETH9', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
-
+    let wethContract = weth;
     current = await wethContract.connect(addr2).deposit({ value: hre.ethers.utils.parseEther('200') });
     await current.wait();
     current = await wethContract.connect(addr2).approve(trader.address, hre.ethers.utils.parseEther('200'));
@@ -236,37 +320,54 @@ async function mainbid() {
     var signature2 = await addr2._signTypedData(domain, types, order);
     //   console.log("21",signature2)
 
-    order.signature = signature2;
+    var signature = signature2.substring(2);
+    order.r = '0x' + signature.substring(0, 64);
+    order.s = '0x' + signature.substring(64, 128);
+    order.v = parseInt(signature.substring(128, 130), 16);
 
-    console.log(order);
+
+    // console.log(order);
 
     var postPay = 0;
     //   const recoveredAddress = hre.ethers.utils.verifyTypedData(domain, types, order, signature2);
     //   console.log(recoveredAddress)
     //   // var signedMatch = ["0x30917a657ae7d1132bdca40187d781fa3b60002f",2608,"0x55ca81f5f00dee4a072f793d67296abd6b56ba0b",100000000000,[1000000,"0xcd105202276e97b531065a087cecf8f0b76ab737"],[1000000,"0xcd105202276e97b531065a087cecf8f0b76ab737"],true,1000000,20,1655555,27,"0xd3dc3475099d1f59fa88de9d0d547a6b26a5e38fb210f0bdded377e089d3eb5c","0xd3dc3475099d1f59fa88de9d0d547a6b26a5e38fb210f0bdded377e089d3eb5c"]
-    var d = await trader.connect(addr1).fillBid(order, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16']);
+    // Order calldata o,
+    // uint256 amount,
+    // address referrer,
+    // Payment calldata p
 
+    
+    var d = await trader.connect(addr1).fillBid(order, 1, null_address, [0, "0x6067D233D5eA619d464a218eAf9921B9343e4d16"]);
+    console.log(":P")
     receipt = await d.wait();
     console.log(receipt.cumulativeGasUsed);
 
-    receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    receipt = await (await gameitem.mint(addr1.address)).wait();
     tokenid = parseInt(receipt.events[0].args[2]);
     order.tokenId = tokenid;
     signature2 = await addr2._signTypedData(domain, types, order);
-    order.signature = signature2;
+    var signature = signature2.substring(2);
+    order.r = '0x' + signature.substring(0, 64);
+    order.s = '0x' + signature.substring(64, 128);
+    order.v = parseInt(signature.substring(128, 130), 16);
 
-    d = await trader.connect(addr1).fillBid(order, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16']);
+    d = await trader.connect(addr1).fillBid(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16']);
     receipt = await d.wait();
-    console.log(receipt.cumulativeGasUsed);
+    console.log(parseInt(receipt.cumulativeGasUsed));
 
-    receipt = await (await gameitem.awardItem(addr1.address)).wait();
+    receipt = await (await gameitem.mint(addr1.address)).wait();
     tokenid = parseInt(receipt.events[0].args[2]);
     order.tokenId = tokenid;
     signature2 = await addr2._signTypedData(domain, types, order);
-    order.signature = signature2;
-    d = await trader.connect(addr1).fillBid(order, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16']);
+    var signature = signature2.substring(2);
+    order.r = '0x' + signature.substring(0, 64);
+    order.s = '0x' + signature.substring(64, 128);
+    order.v = parseInt(signature.substring(128, 130), 16);
+
+    d = await trader.connect(addr1).fillBid(order, 1, null_address, [postPay, '0x6067D233D5eA619d464a218eAf9921B9343e4d16']);
     receipt = await d.wait();
-    console.log(receipt.cumulativeGasUsed);
+    console.log(parseInt(receipt.cumulativeGasUsed));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
